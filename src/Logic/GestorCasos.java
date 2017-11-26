9d5b48b21c7de3637203a26143933812a6430978
JoseSolano <jsolanom>
2017-11-26 10:34:25 -0600

    Gestor Casos

--- src/Logic/GestorCasos.java
+++ src/Logic/GestorCasos.java
@@ -1,6 +1,7 @@
 package Logic;
 
 import Business.Caso.Caso;
+import Business.Caso.DetalleHistorial;
 import Business.Caso.Estado;
 import Business.Persona.Juez;
 import Business.Persona.Querellante;
@@ -10,11 +11,14 @@ import java.io.IOException;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ComboBox;
 
 public class GestorCasos {
 
     CasosService casosService = new CasosService();
-
+    
     public ArrayList<String[]> getQuerellantes(int id) throws IOException, SQLException {
 
         ArrayList<Caso> listQuere = casosService.getCasosPorQuerellante(id);
@@ -56,16 +60,132 @@ public class GestorCasos {
         Querellante qr= new Querellante();
         Estado est = new Estado();
         est.setIdEstado(1);
-        
+
         qr.setIdPersona(id);
         cas.setDenunciante(qr);
         cas.setDescripcion(desc);
         cas.setFechaCfeacion(new Date());
         cas.setEstadoCaso(est);
         cas.setJuezAsignado(ju);
-        
+
         casosService.create(cas);
-        
     }
 
+    public ArrayList<String[]> getCasosByJuez(int id) throws IOException, SQLException {
+
+        ArrayList<Caso> listQuere = casosService.getCasosPorJuez(id);
+        ArrayList<String[]> casesResult = new ArrayList<>();
+        String[] casosInfo;
+        for (Caso quere : listQuere) {
+            casosInfo = new String[5];
+            casosInfo[0] = quere.getNumeroCaso() + "";
+            casosInfo[1] = quere.getDescripcion() + "";
+            casosInfo[2] = quere.getFechaCfeacion().toString();
+            casosInfo[3] = quere.getDenunciante().getNombre() + " " + quere.getDenunciante().getApellido();
+            casosInfo[4] = quere.getEstadoCaso().getNombre();
+            casesResult.add(casosInfo);
+        }
+        return casesResult;
+    }
+
+    public  List getCheckBox(String CurrentStatus, String casoId)
+    {
+        ChoiceBox choiceBox = new ChoiceBox();
+         List result = new ArrayList<String>();
+        switch(CurrentStatus)
+        {
+            case "Recibido":
+                choiceBox.getItems().addAll("Recibido","Aceptado","Rechazado");
+                result.add("Recibido");               
+                result.add("Aceptado");
+                result.add("Rechazado");
+            break;
+            case "Consulta":
+                choiceBox.getItems().addAll("Aceptado","Consulta","Rechazado");
+                result.add("Aceptado");
+                result.add("Consulta");
+                result.add("Rechazado");
+            break;
+            case "Aceptado":
+                choiceBox.getItems().addAll("Aceptado","Redactado");
+                result.add("Aceptado");
+                result.add("Redactado");
+            break;
+            case "Redactado":
+                choiceBox.getItems().addAll("Redactado","Revisión","Resuleto");
+                result.add("Redactado");
+                result.add("Revisión");
+                result.add("Resuleto");
+            break;
+            case "Revisión":
+                choiceBox.getItems().addAll("Revisión","Redactado");
+                result.add("Redactado");
+                result.add("Revisión");
+            break;
+            default:
+                choiceBox.getItems().addAll(CurrentStatus);
+                result.add(CurrentStatus);
+            break;
+        }
+        return result;
+    }
+
+    public void updateEstadoCaso(String idCaso, String estado) throws SQLException, IOException
+    {
+        int nuevoEstdo = 1;
+
+        switch(estado)
+        {
+            case "Recibido":
+                nuevoEstdo = 1;
+            break;
+            case "Consulta":
+                nuevoEstdo = 2;
+            break;
+            case "Aceptado":
+                nuevoEstdo = 3;
+            break;
+            case "Redactado":
+                nuevoEstdo = 4;
+            break;
+            case "Revisión":
+                nuevoEstdo = 5;
+            break;
+            case "Resuleto":
+                nuevoEstdo = 6;
+            break;
+            case "Rechazado":
+                nuevoEstdo = 7;
+            break;
+        }
+        casosService.actualizarEstado(Integer.parseInt(idCaso), nuevoEstdo);
+        casosService.createHistorial(nuevoEstdo, Integer.parseInt(idCaso));
+    }
+     public String[] getCaso(int id) throws IOException, SQLException {
+         Caso quere = casosService.getCasos(id);
+        String[] casosInfo;
+       
+            casosInfo = new String[5];
+            casosInfo[0] = quere.getNumeroCaso() + "";
+            casosInfo[1] = quere.getDescripcion() + "";
+            casosInfo[2] = quere.getFechaCfeacion().toString();
+            casosInfo[3] = quere.getDenunciante().getNombre() + " " + quere.getDenunciante().getApellido();
+            casosInfo[4] = quere.getEstadoCaso().getNombre();
+       
+        return casosInfo;
+    }
+     
+     public ArrayList<String[]> getHistorialCaso(int id) throws IOException, SQLException 
+     {
+        ArrayList<DetalleHistorial> listQuere = casosService.getCasoId(id);
+        ArrayList<String[]> casesResult = new ArrayList<>();
+        String[] casosInfo;
+        for (DetalleHistorial detalle : listQuere) {
+            casosInfo = new String[2];
+            casosInfo[0] = detalle.getFechaActualizacion() + "";
+            casosInfo[1] = detalle.getEstadoHistorial().getNombre();
+            casesResult.add(casosInfo);
+        }
+        return casesResult;
+    }
 }
